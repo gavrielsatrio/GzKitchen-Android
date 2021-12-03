9b5f511cd85896d3b961c36022e2fe53c4059591
competitor1 <46616416+gavrielsatrio@users.noreply.github.com>
2021-12-03 12:22:28 +0700

    Edit SignUp Activity
    User can now sign up

--- app/src/main/java/com/example/gzkitchen/SignUpActivity.java
+++ app/src/main/java/com/example/gzkitchen/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.example.gzkitchen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaCodec;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.Inet4Address;
+import java.util.regex.Matcher;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -47,11 +57,54 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUpActivity.this, MemberMainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                if(!txtName.getText().toString().trim().equals("") && !txtEmail.getText().toString().trim().equals("") && !txtPassword.getText().toString().trim().equals("") && !txtConfirmPassword.getText().toString().trim().equals("")) {
+                    if(Patterns.EMAIL_ADDRESS.matcher(txtEmail.getText().toString().trim()).matches()) {
+                        if(txtPassword.getText().toString().trim().length() >= 8) {
+                            if(txtConfirmPassword.getText().toString().trim().equals(txtPassword.getText().toString().trim())) {
+                                SharedPreferences sharedPref = getSharedPreferences("AppLocalData", Context.MODE_PRIVATE);
+                                try {
+                                    JSONArray jsonArrayUsers = new JSONArray(sharedPref.getString("", "defaultValue"));
+                                    boolean isEmailAlreadyExists = false;
+                                    for(int i = 0; i < jsonArrayUsers.length(); i++) {
+                                        JSONObject objectUser = jsonArrayUsers.getJSONObject(i);
+                                        if(txtEmail.getText().toString().trim().equals(objectUser.getString("Email"))) {
+                                            isEmailAlreadyExists = true;
+                                            break;
+                                        }
+                                    }
 
-                finish();
+                                    if(isEmailAlreadyExists == false) {
+                                        jsonArrayUsers.put(new JSONObject()
+                                                .put("Name", txtName.getText().toString().trim())
+                                                .put("Email", txtEmail.getText().toString().trim())
+                                                .put("Password", txtPassword.getText().toString().trim())
+                                                .put("Role", "Member")
+                                        );
+                                        sharedPref.edit().putString("Users", jsonArrayUsers.toString()).apply();
+
+                                        Intent intent = new Intent(SignUpActivity.this, MemberMainActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                        startActivity(intent);
+
+                                        finish();
+                                    } else {
+                                        Toast.makeText(SignUpActivity.this, "This email is already registered", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "Confirm password doesn't match with the password", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Password must contains at least 8 characters", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Email format is not valid", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Please fill up all information", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
