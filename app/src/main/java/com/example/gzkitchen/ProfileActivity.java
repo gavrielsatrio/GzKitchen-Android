2345a29a3f90aed57d0774c2adbf3abb8ba6934c
competitor1 <46616416+gavrielsatrio@users.noreply.github.com>
2021-12-04 14:53:57 +0700

    Add animation: Profile Activity

--- app/src/main/java/com/example/gzkitchen/ProfileActivity.java
+++ app/src/main/java/com/example/gzkitchen/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.gzkitchen;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,8 +11,10 @@ import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,13 +23,18 @@ import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    ImageView imgBackgroundTop;
+    CardView cardViewBackground;
+    ImageView imgProfile;
     Button btnLogout;
     TextView lblName;
     TextView lblRole;
     TextView lblEmail;
     TextView btnBack;
     Button btnEditProfile;
+    ImageView imgBtnEditProfile;
     Button btnAboutDeveloper;
+    ImageView imgBtnAboutDeveloper;
 
     JSONObject objectUser = new JSONObject();
 
@@ -35,15 +43,21 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        imgBackgroundTop = findViewById(R.id.profileImgTop);
+        cardViewBackground = findViewById(R.id.profileCardView);
+        imgProfile = findViewById(R.id.profileImgProfile);
         btnLogout = findViewById(R.id.profileBtnLogout);
         lblName = findViewById(R.id.profileLblName);
         lblRole = findViewById(R.id.profileLblRole);
         lblEmail = findViewById(R.id.profileLblEmail);
         btnBack = findViewById(R.id.profileBtnBack);
         btnEditProfile = findViewById(R.id.profileBtnEditProfile);
+        imgBtnEditProfile = findViewById(R.id.profileImgEditProfile);
         btnAboutDeveloper = findViewById(R.id.profileBtnAboutDeveloper);
+        imgBtnAboutDeveloper = findViewById(R.id.profileImgAboutDeveloper);
 
         LoadData();
+        LoadAnimation();
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,6 +142,26 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void LoadAnimation() {
+        imgBackgroundTop.animate().setDuration(800).alpha(1).translationY(0).setInterpolator(new DecelerateInterpolator());
+
+        btnBack.animate().setDuration(600).setStartDelay(120).alpha(1).translationX(0);
+        cardViewBackground.animate().setDuration(600).setStartDelay(240).alpha(1).translationY(0);
+        imgProfile.animate().setDuration(600).setStartDelay(360).alpha(1).translationY(0);
+
+        lblName.animate().setDuration(600).setStartDelay(480).alpha(1).translationY(0);
+        lblRole.animate().setDuration(600).setStartDelay(600).alpha(1).translationX(0);
+        lblEmail.animate().setDuration(600).setStartDelay(720).alpha(1).translationX(0);
+
+        btnEditProfile.animate().setDuration(600).setStartDelay(840).alpha(1).translationY(0);
+        imgBtnEditProfile.animate().setDuration(600).setStartDelay(840).alpha(1).translationY(0);
+
+        btnAboutDeveloper.animate().setDuration(600).setStartDelay(960).alpha(1).translationY(0);
+        imgBtnAboutDeveloper.animate().setDuration(600).setStartDelay(960).alpha(1).translationY(0);
+
+        btnLogout.animate().setDuration(600).setStartDelay(1080).alpha(1).translationY(0);
     }
 
     private void LoadData() {
