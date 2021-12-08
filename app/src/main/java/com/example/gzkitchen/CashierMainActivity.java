942891768c96cce366764640c40cbc01370f6a4f
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2021-12-08 13:09:24 +0700

    Add CashierMain Activity

---
+++ app/src/main/java/com/example/gzkitchen/CashierMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gzkitchen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashierMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cashier_main);
+    }
+}
