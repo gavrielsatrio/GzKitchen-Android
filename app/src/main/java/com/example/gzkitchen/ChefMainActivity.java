68774402d7631ad0eefdc3cd2c6e204f4756bb84
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2021-12-08 13:08:36 +0700

    Add ChefMain Activity

---
+++ app/src/main/java/com/example/gzkitchen/ChefMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gzkitchen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChefMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_main);
+    }
+}
