5ddf66b134c5a43f72e785393acb7d8bd9cede92
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2021-12-14 22:07:51 +0700

    Edit CashierTakeOrder Activity
    Design TakeOrder Menu Item Layout

--- app/src/main/java/com/example/gzkitchen/CashierMainTakeOrderFragment.java
+++ app/src/main/java/com/example/gzkitchen/CashierMainTakeOrderFragment.java
@@ -4,12 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class CashierMainTakeOrderFragment extends Fragment {
     CashierMainActivity cashierMainActivity;
     View viewInflate;
+
+    TextView lblHeader;
+    EditText txtSearch;
+    ImageView btnSearch;
+    LinearLayout linearLayoutOrderMenuList;
+    CardView cardViewBottom;
+    TextView lblPrice;
+    Button btnPay;
 
     public CashierMainTakeOrderFragment(CashierMainActivity cashierMainActivityParam) {
         this.cashierMainActivity = cashierMainActivityParam;
@@ -18,6 +32,17 @@ public class CashierMainTakeOrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         viewInflate = inflater.inflate(R.layout.cashier_main_take_order_layout, container, false);
+
+        lblHeader = viewInflate.findViewById(R.id.cashierMainTakeOrderLblHeader);
+        txtSearch = viewInflate.findViewById(R.id.cashierMainTakeOrderTxtSearch);
+        btnSearch = viewInflate.findViewById(R.id.cashierMainTakeOrderBtnSearch);
+        linearLayoutOrderMenuList = viewInflate.findViewById(R.id.cashierMainTakeOrderLinearLayoutOrderMenuList);
+        cardViewBottom = viewInflate.findViewById(R.id.cashierMainTakeOrderCardViewBottom);
+        lblPrice = viewInflate.findViewById(R.id.cashierMainTakeOrderLblPrice);
+        btnPay = viewInflate.findViewById(R.id.cashierMainTakeOrderBtnProceedPayment);
+
+        View viewOrderMenu = LayoutInflater.from(cashierMainActivity).inflate(R.layout.take_order_layout, null, false);
+        linearLayoutOrderMenuList.addView(viewOrderMenu);
 
         return viewInflate;
     }
