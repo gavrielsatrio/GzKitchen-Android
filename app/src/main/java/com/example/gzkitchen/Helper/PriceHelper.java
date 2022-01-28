09000aae22632a0d165f00dff5b3e335992105af
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2022-01-28 16:10:47 +0700

    Update : Fix conditional error
    - Price isn't in Rupiah in some devices
    - Menu image won't load when browsed by admin in the AddEdit Activity

--- app/src/main/java/com/example/gzkitchen/Helper/PriceHelper.java
+++ app/src/main/java/com/example/gzkitchen/Helper/PriceHelper.java
@@ -1,10 +1,11 @@
 package com.example.gzkitchen.Helper;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class PriceHelper {
     public String convertToRupiah(int price) {
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
         formatter.setMaximumFractionDigits(0);
 
         return formatter.format(price).replace("$", "Rp");
